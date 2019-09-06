a8cfa9b3c897c00be2c46666d5e0dd8bbf45d96d
yuxiaohuang <yuxiaohuang@gwu.edu>
2019-09-06 10:40:34 -0400

    add fid

---
+++ gwu/submitted/fid/code/python/fiblr/pipeline.py
@@ -0,0 +1,273 @@
+# Please cite the following paper when using the code
+
+import sys
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import Setting
+import FIBLR
+
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import StratifiedKFold
+from joblib import Parallel, delayed
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+def pipeline_all_datasets():
+    """
+    The pipeline for all data sets
+    :return:
+    """
+
+    # Add code_dir folder
+    sys.path.append(dp_dir)
+
+    # Import DataPreprocessing module
+    import DataPreprocessing
+    dp = DataPreprocessing.DataPreprocessing(data_dir)
+
+    # Match data files with names file
+    data_names = dp.match_data_names()
+
+    # The pipeline for each data set (in parallel)
+    # Set backend="multiprocessing" (default) to prevent sharing memory between parent and threads
+    Parallel(n_jobs=1)(delayed(pipeline_one_dataset)(dp, data_files, names_file)
+                       for data_files, names_file in data_names)
+
+
+def pipeline_one_dataset(dp, data_files, names_file):
+    """
+    The pipeline for one data set
+    :param dp: the DataPreprocessing module
+    :param data_files: the pathname of the data files
+    :param names_file: the pathname of the names file
+    :return:
+    """
+
+    # Data preprocessing: get the Setting, Names, and Data object
+    setting, names, data = dp.get_setting_names_data(data_files, names_file, result_dir, Setting)
+
+    # Get the sklearn pipeline
+    pipe_fiblr = Pipeline([('scaler', setting.scaler),
+                            ('fiblr', FIBLR.FIBLR(setting.max_iter,
+                                                  setting.bin_num_percent,
+                                                  setting.min_bin_num,
+                                                  setting.max_bin_num,
+                                                  setting.eta,
+                                                  setting.tol,
+                                                  setting.random_state,
+                                                  setting.n_jobs))])
+
+    # Hyperparameter tuning using GridSearchCV
+    gs = GridSearchCV(estimator=pipe_fiblr,
+                      param_grid=[{'fiblr__bin_num_percent': setting.bin_num_percents,
+                                   'fiblr__eta': setting.etas}],
+                      scoring=setting.scoring,
+                      n_jobs=setting.n_jobs,
+                      cv=StratifiedKFold(n_splits=setting.n_splits,
+                                         random_state=setting.random_state))
+    gs.fit(data.X, data.y)
+
+    # Get the results
+    get_results(setting, names, data, gs)
+
+
+def get_results(setting, names, data, gs):
+    """
+    Get the results
+    :param setting: the Setting object
+    :param names: the Names object
+    :param data: the Data object
+    :param gs: the GridSearchCV object
+    :return:
+    """
+
+    if setting.prob_dists_fig_dir is not None:
+        # Plot the probability distribution figures
+        plot_prob_dists_fig(setting, names, data.X, gs.best_estimator_.named_steps['fiblr'])
+
+    if setting.prob_dists_file_dir is not None:
+        # Write the probability distribution file
+        write_prob_dists_file(setting, names, data.X, gs.best_estimator_.named_steps['fiblr'])
+
+    if setting.cv_results_file_dir is not None:
+        # Write the cv results file
+        write_cv_results_file(setting, gs.cv_results_)
+
+    if setting.best_params_file_dir is not None:
+        # Write the best hyperparameters file
+        write_best_params_file(setting, gs.best_params_)
+
+
+def plot_prob_dists_fig(setting, names, X, fiblr):
+    """
+    Plot the probability distribution figures.
+    :param setting: the Setting object
+    :param names: the Names object
+    :param X: the feature matrix
+    :param fiblr: the fiblr model
+    :return:
+    """
+
+    # Make directory
+    directory = os.path.dirname(setting.prob_dists_fig_dir)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Set plt
+    setting.set_plt()
+
+    for class_ in sorted(fiblr.weights.keys()):
+        # Get the original value of class_ before the encoding
+        class_ori = str(setting.encoder.inverse_transform(np.array([class_]))[0])
+
+        for j in sorted(fiblr.prob_dists[class_].keys()):
+            # Get the name of the jth feature
+            xj_name = names.features[j]
+
+            # Get the unique value of the jth feature and their indices
+            xijs, is_ = np.unique(X[:, j], return_index=True)
+
+            # Get the pairs of the value of the jth feature and their indices
+            xij_i_pairs = list(zip(xijs, is_))
+
+            # Sort the pairs above in ascending order of the value of the jth feature
+            xij_i_pairs_sorted = sorted(xij_i_pairs, key=lambda x: x[0])
+
+            # Get the sorted value of the jth feature
+            xijs_sorted = [xij for xij, i in xij_i_pairs_sorted]
+
+            # Get the corresponding probabilities
+            pijs = [fiblr.prob_dists[class_][j][i] for xij, i in xij_i_pairs_sorted]
+
+            # Get the pandas dataframe
+            df = pd.DataFrame(list(zip(xijs_sorted, pijs)), columns=[xj_name, 'Probability'])
+
+            # Plot the histogram
+            df.plot(x=xj_name,
+                    y='Probability',
+                    kind='bar',
+                    yticks=[0, 0.25, 0.5, 0.75, 1],
+                    ylim=(0, 1),
+                    figsize=(20, 10),
+                    title=class_ori,
+                    legend=False,
+                    color='b')
+
+            # Set the x-axis label
+            plt.xlabel(xj_name)
+            # Set the y-axis label
+            plt.ylabel('Probability')
+
+            if len(xijs_sorted) > 50:
+                plt.tick_params(labelbottom=False)
+
+            plt.tight_layout()
+            prob_dists_fig = (setting.prob_dists_fig_dir + setting.prob_dists_fig_name + '_' + class_ori + '_' + xj_name
+                             + setting.prob_dists_fig_type)
+            plt.savefig(prob_dists_fig)
+
+
+def write_prob_dists_file(setting, names, X, fiblr):
+    """
+    Write the probability distribution file
+    :param setting: the Setting object
+    :param names: the Names object
+    :param X: the feature matrix
+    :param fiblr: the fiblr model
+    :return:
+    """
+
+    # Make directory
+    directory = os.path.dirname(setting.prob_dists_file_dir)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    prob_dists_file = setting.prob_dists_file_dir + setting.prob_dists_file_name + setting.prob_dists_file_type
+
+    with open(prob_dists_file, 'w') as f:
+        # Write header
+        f.write("Class,Feature,Value,Probability" + '\n')
+
+        for class_ in sorted(fiblr.weights.keys()):
+            # Get the original value of class_ before the encoding
+            class_ori = str(setting.encoder.inverse_transform(np.array([class_]))[0])
+
+            for j in sorted(fiblr.prob_dists[class_].keys()):
+                # Get the name of the jth feature
+                xj_name = names.features[j]
+
+                # Get the unique value of the jth feature and their indices
+                xijs, is_ = np.unique(X[:, j], return_index=True)
+
+                # Get the pairs of the value of the jth feature and their indices
+                xij_i_pairs = list(zip(xijs, is_))
+
+                # Sort the pairs above in ascending order of the value of the jth feature
+                xij_i_pairs_sorted = sorted(xij_i_pairs, key=lambda x: x[0])
+
+                # Get the sorted value of the jth feature
+                xijs_sorted = [xij for xij, i in xij_i_pairs_sorted]
+
+                # Get the corresponding probabilities
+                pijs = [fiblr.prob_dists[class_][j][i] for xij, i in xij_i_pairs_sorted]
+
+                for xij, pij in list(zip(xijs_sorted, pijs)):
+                    f.write(class_ori + ',' + xj_name + ',' + str(xij) + ',' + str(pij) + '\n')
+
+
+def write_cv_results_file(setting, cv_results):
+    """
+    Write the cv results file
+    :param setting: the Setting object
+    :param cv_results: the cv results
+    :return:
+    """
+
+    # Make directory
+    directory = os.path.dirname(setting.cv_results_file_dir)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    cv_results_file = setting.cv_results_file_dir + setting.cv_results_file_name + setting.cv_results_file_type
+
+    # Sort cv_results in ascending order of 'rank_test_score' and 'std_test_score'
+    cv_results = pd.DataFrame.from_dict(cv_results).sort_values(by=['rank_test_score', 'std_test_score'])
+
+    cv_results.to_csv(path_or_buf=cv_results_file)
+
+
+def write_best_params_file(setting, best_params):
+    """
+    Write the best hyperparameters file
+    :param setting: the Setting object
+    :param best_params: the best hyperparameters
+    :return:
+    """
+
+    # Make directory
+    directory = os.path.dirname(setting.best_params_file_dir)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    best_params_file = setting.best_params_file_dir + setting.best_params_file_name + setting.best_params_file_type
+
+    pd.Series(best_params).to_csv(path=best_params_file)
+
+
+if __name__ == "__main__":
+    # Get the pathname of the data directory from command line
+    data_dir = sys.argv[1]
+
+    # Get the pathname of the result directory from command line
+    result_dir = sys.argv[2]
+
+    # Get the pathname of the DataPreprocessing module directory
+    dp_dir = sys.argv[3]
+
+    # The pipeline for all data sets
+    pipeline_all_datasets()
---
+++ gwu/submitted/fid/code/supplementary/python/print_table_acc.py
@@ -0,0 +1,159 @@
+import os
+import sys
+import csv
+import glob
+import pandas as pd
+import numpy as np
+
+from scipy.stats import ttest_ind_from_stats
+
+
+def print_table_acc():
+    """
+    Print table (accuracy)
+    :return:
+    """
+
+    datasets = {'iris': 'Iris',
+                'parkinsons': 'Parkinson\'s',
+                'drug_consumption': 'Drug consumption',
+                'drosophila_enhancers': 'Drosophila enhancers',
+                'alternative_splicing': 'Alternative splicing'}
+
+    methods = {'LogisticRegression': 'LR',
+               'GaussianNB': 'GNB',
+               'fiblr': 'FIBLR'}
+
+    files = glob.glob(result_dir + '**/*.csv', recursive=True)
+
+    # Get the score file of others
+    files_others = [file for file in files if
+                    '/cv_results_file/' in os.path.abspath(file)]
+
+    # Get the score file of fiblr
+    files_fiblr = [file for file in files if
+                   '/fiblr/' in os.path.abspath(file) and '/cv_results_file/' in os.path.abspath(file)]
+
+    # Make directory
+    directory = os.path.dirname(table_file)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Write the table file
+    with open(table_file, 'w') as f:
+        # Table content
+        # Write the methods
+        content = 'Dataset' + '\t' + '&' + '\t&'.join([methods[method] for method in methods.keys()]) + '\\\\'
+        f.write(content + '\n')
+
+        for dataset in datasets.keys():
+            content = datasets[dataset] + '\t' + '&'
+            scores = []
+
+            for method in methods.keys():
+                dataset_method_score_files_others = get_score_files(files_others, dataset, method)
+                dataset_method_score_files_fiblr = get_score_files(files_fiblr, dataset, '')
+
+                if len(dataset_method_score_files_others) > 0:
+                    nobs_others, mean_others, std_others = get_nobs_mean_std(dataset_method_score_files_others)
+                else:
+                    nobs_others, mean_others, std_others = None, None, None
+
+                if len(dataset_method_score_files_fiblr) > 0:
+                    nobs_fiblr, mean_fiblr, std_fiblr = get_nobs_mean_std(dataset_method_score_files_fiblr)
+                else:
+                    nobs_fiblr, mean_fiblr, std_fiblr = None, None, None
+
+                if mean_others is not None and mean_fiblr is not None:
+                    dif = mean_others - mean_fiblr
+
+                    if dif < 0:
+                        score = str(format(mean_others * 100, '.' + str(num_decimal_places) + 'f')) + '\\%'
+                    elif dif > 0:
+                        score = str(format(mean_others * 100, '.' + str(num_decimal_places) + 'f')) + '\\%'
+                    else:
+                        score = str(format(mean_others * 100, '.' + str(num_decimal_places) + 'f')) + '\\%'
+
+                    if (std_others is not None
+                            and std_fiblr is not None
+                            and std_others + std_fiblr != 0):
+                        # significance test
+                        statistic, pvalue = ttest_ind_from_stats(mean1=mean_others,
+                                                                 std1=std_others,
+                                                                 nobs1=nobs_others,
+                                                                 mean2=mean_fiblr,
+                                                                 std2=std_fiblr,
+                                                                 nobs2=nobs_fiblr,
+                                                                 equal_var=False)
+
+                        if statistic < 0 and pvalue < p_val:
+                            score += ' $\\blacktriangle$'
+                        elif statistic > 0 and pvalue < p_val:
+                            score += ' $\\blacktriangledown$'
+                else:
+                    score = ''
+
+                scores.append(score)
+
+            content += '\t&'.join(scores) + '\\\\'
+            f.write(content + '\n')
+
+
+def get_score_files(files, dataset, method):
+    """
+    Get the score files containing keywords dataset and method
+    :param files: the score files
+    :param dataset: the name of the dataset
+    :param method: the name of the method
+    :return: the score files containing keywords dataset and method
+    """
+
+    score_files = []
+
+    for file in files:
+        if os.path.exists(file) is True:
+            for str_ in os.path.abspath(file).split('/'):
+                if str_ == dataset:
+                    for str_ in os.path.abspath(file).split('/'):
+                        if str_ == method:
+                            score_files.append(file)
+
+    return score_files
+
+
+def get_nobs_mean_std(score_files):
+    """
+    Get the number of observations, mean and std
+    :param score_files: the score files
+    :return: the number of observations, mean and std
+    """
+
+    scores = []
+
+    for score_file in score_files:
+        # Get the cv results
+        df = pd.read_csv(score_file, header=0)
+
+        # Add the test score from each fold to scores
+        for col in df.columns:
+            if 'split' in col and 'test_score' in col:
+                scores.append(df[col][0])
+
+    return [len(scores), np.mean(scores), np.std(scores)]
+
+
+if __name__ == "__main__":
+    # Get the pathname of the result directory from command line
+    result_dir = sys.argv[1]
+
+    # Get the number of decimal places
+    num_decimal_places = int(sys.argv[2])
+
+    # Get the p-value from command line
+    p_val = float(sys.argv[3])
+
+    # Get the pathname of the table file
+    table_file = sys.argv[4]
+
+    # Print table (accuracy)
+    print_table_acc()
